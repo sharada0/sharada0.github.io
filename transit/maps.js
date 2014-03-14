df6a0c0500a48674d9036430415a7b0fa77e4bbb
Sharada <sharada>
2014-03-13 23:02:22 -0400

    fixed parens

--- transit/maps.js
+++ transit/maps.js
@@ -37,58 +37,59 @@ function getMyLocation()
 	}
 }
 
-			function renderMap()
-			{
-				me = new google.maps.LatLng(myLat, myLng);
+function renderMap()
+{
+	me = new google.maps.LatLng(myLat, myLng);
 
-				// Update map and go there...
-				map.panTo(me);
+	// Update map and go there...
+	map.panTo(me);
 
-				// Create a marker
-				marker = new google.maps.Marker({
-					position: me,
-					title: "Here I Am!"
-				});
-				marker.setMap(map);
+	// Create a marker
+	marker = new google.maps.Marker({
+		position: me,
+		title: "Here I Am!"
+	});
+	marker.setMap(map);
 
-				// Open info window on click of marker
-				google.maps.event.addListener(marker, 'click', function() {
-					infowindow.setContent(marker.title);
-					infowindow.open(map, marker);
-				});
+	// Open info window on click of marker
+	google.maps.event.addListener(marker, 'click', function() {
+		infowindow.setContent(marker.title);
+		infowindow.open(map, marker);
+	});
 
-				// Calling Google Places API
-				var request = {
-					location: me,
-					radius: '500',
-					types: ['food']
-				};
-				service = new google.maps.places.PlacesService(map);
-				service.search(request, callback);
-			}
+	// Calling Google Places API
+	var request = {
+		location: me,
+		radius: '500',
+		types: ['food']
+	};
+	service = new google.maps.places.PlacesService(map);
+	service.search(request, callback);
+}
 
-			// Taken from http://code.google.com/apis/maps/documentation/javascript/places.html
-			function callback(results, status)
-			{
-				if (status == google.maps.places.PlacesServiceStatus.OK) {
-					alert("Got places back!");
-					places = results;
-					for (var i = 0; i < results.length; i++) {
-						createMarker(results[i]);
-					}
-				}
-			}
+// Taken from http://code.google.com/apis/maps/documentation/javascript/places.html
+function callback(results, status)
+{
+	if (status == google.maps.places.PlacesServiceStatus.OK) {
+		alert("Got places back!");
+		places = results;
+		for (var i = 0; i < results.length; i++) {
+			createMarker(results[i]);
+		}
+	}
+}
 
-			function createMarker(place)
-			{
-				var placeLoc = place.geometry.location;
-				var marker = new google.maps.Marker({
+function createMarker(place)
+{
+	var placeLoc = place.geometry.location;
+	var marker = new google.maps.Marker({
 					map: map,
 					position: place.geometry.location
-				});
+	});
 
-				google.maps.event.addListener(marker, 'click', function() {
+	google.maps.event.addListener(marker, 'click', function() {
 					infowindow.close();
 					infowindow.setContent(place.name);
 					infowindow.open(map, this);
-				});
+	});
+}
