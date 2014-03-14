a04e0d6241b6758028743bb9e6b74618fc083c8e
Sharada <sharada>
2014-03-14 16:06:01 -0400

    changed long to Long

--- transit/maps.js
+++ transit/maps.js
@@ -20,319 +20,319 @@ var stations = [
     "Line":"Blue",
     "station":"Airport",
     "lat":42.374262,
-    "long":-71.030395
+    "Long":-71.030395
   },
   {
     "Line":"Blue",
     "station":"Aquarium",
     "lat":42.359784,
-    "long":-71.051652
+    "Long":-71.051652
   },
   {
     "Line":"Blue",
     "station":"Beachmont",
     "lat":42.39754234,
-    "long":-70.99231944
+    "Long":-70.99231944
   },
   {
     "Line":"Blue",
     "station":"Bowdoin",
     "lat":42.361365,
-    "long":-71.062037
+    "Long":-71.062037
   },
   {
     "Line":"Blue",
     "station":"Government Center",
     "lat":42.359705,
-    "long":-71.059215
+    "Long":-71.059215
   },
   {
     "Line":"Blue",
     "station":"Maverick",
     "lat":42.36911856,
-    "long":-71.03952958
+    "Long":-71.03952958
   },
   {
     "Line":"Blue",
     "station":"Orient Heights",
     "lat":42.386867,
-    "long":-71.004736
+    "Long":-71.004736
   },
   {
     "Line":"Blue",
     "station":"Revere Beach",
     "lat":42.40784254,
-    "long":-70.99253321
+    "Long":-70.99253321
   },
   {
     "Line":"Blue",
     "station":"State Street",
     "lat":42.358978,
-    "long":-71.057598
+    "Long":-71.057598
   },
   {
     "Line":"Blue",
     "station":"Suffolk Downs",
     "lat":42.39050067,
-    "long":-70.99712259
+    "Long":-70.99712259
   },
   {
     "Line":"Blue",
     "station":"Wonderland",
     "lat":42.41342,
-    "long":-70.991648
+    "Long":-70.991648
   },
   {
     "Line":"Blue",
     "station":"Wood Island",
     "lat":42.3796403,
-    "long":-71.02286539
+    "Long":-71.02286539
   },
   {
     "Line":"Orange",
     "station":"Back Bay",
     "lat":42.34735,
-    "long":-71.075727
+    "Long":-71.075727
   },
   {
     "Line":"Orange",
     "station":"Chinatown",
     "lat":42.352547,
-    "long":-71.062752
+    "Long":-71.062752
   },
   {
     "Line":"Orange",
     "station":"Community College",
     "lat":42.373622,
-    "long":-71.069533
+    "Long":-71.069533
   },
   {
     "Line":"Orange",
     "station":"Downtown Crossing",
     "lat":42.355518,
-    "long":-71.060225
+    "Long":-71.060225
   },
   {
     "Line":"Orange",
     "station":"Forest Hills",
     "lat":42.300523,
-    "long":-71.113686
+    "Long":-71.113686
   },
   {
     "Line":"Orange",
     "station":"Green Street",
     "lat":42.310525,
-    "long":-71.107414
+    "Long":-71.107414
   },
   {
     "Line":"Orange",
     "station":"Haymarket",
     "lat":42.363021,
-    "long":-71.05829
+    "Long":-71.05829
   },
   {
     "Line":"Orange",
     "station":"Jackson Square",
     "lat":42.323132,
-    "long":-71.099592
+    "Long":-71.099592
   },
   {
     "Line":"Orange",
     "station":"Malden Center",
     "lat":42.426632,
-    "long":-71.07411
+    "Long":-71.07411
   },
   {
     "Line":"Orange",
     "station":"Mass Ave",
     "lat":42.341512,
-    "long":-71.083423
+    "Long":-71.083423
   },
   {
     "Line":"Orange",
     "station":"North Station",
     "lat":42.365577,
-    "long":-71.06129
+    "Long":-71.06129
   },
   {
     "Line":"Orange",
     "station":"Oak Grove",
     "lat":42.43668,
-    "long":-71.071097
+    "Long":-71.071097
   },
   {
     "Line":"Orange",
     "station":"Roxbury Crossing",
     "lat":42.331397,
-    "long":-71.095451
+    "Long":-71.095451
   },
   {
     "Line":"Orange",
     "station":"Ruggles",
     "lat":42.336377,
-    "long":-71.088961
+    "Long":-71.088961
   },
   {
     "Line":"Orange",
     "station":"State Street",
     "lat":42.358978,
-    "long":-71.057598
+    "Long":-71.057598
   },
   {
     "Line":"Orange",
     "station":"Stony Brook",
     "lat":42.317062,
-    "long":-71.104248
+    "Long":-71.104248
   },
   {
     "Line":"Orange",
     "station":"Sullivan",
     "lat":42.383975,
-    "long":-71.076994
+    "Long":-71.076994
   },
   {
     "Line":"Orange",
     "station":"Tufts Medical",
     "lat":42.349662,
-    "long":-71.063917
+    "Long":-71.063917
   },
   {
     "Line":"Orange",
     "station":"Wellington",
     "lat":42.40237,
-    "long":-71.077082
+    "Long":-71.077082
   },
   {
     "Line":"Red",
     "station":"Alewife",
     "lat":42.395428,
-    "long":-71.142483
+    "Long":-71.142483
   },
   {
     "Line":"Red",
     "station":"Andrew",
     "lat":42.330154,
-    "long":-71.057655
+    "Long":-71.057655
   },
   {
     "Line":"Red",
     "station":"Ashmont",
     "lat":42.284652,
-    "long":-71.064489
+    "Long":-71.064489
   },
   {
     "Line":"Red",
     "station":"Braintree",
     "lat":42.2078543,
-    "long":-71.0011385
+    "Long":-71.0011385
   },
   {
     "Line":"Red",
     "station":"Broadway",
     "lat":42.342622,
-    "long":-71.056967
+    "Long":-71.056967
   },
   {
     "Line":"Red",
     "station":"Central Square",
     "lat":42.365486,
-    "long":-71.103802
+    "Long":-71.103802
   },
   {
     "Line":"Red",
     "station":"Charles/MGH",
     "lat":42.361166,
-    "long":-71.070628
+    "Long":-71.070628
   },
   {
     "Line":"Red",
     "station":"Davis",
     "lat":42.39674,
-    "long":-71.121815
+    "Long":-71.121815
   },
   {
     "Line":"Red",
     "station":"Downtown Crossing",
     "lat":42.355518,
-    "long":-71.060225
+    "Long":-71.060225
   },
   {
     "Line":"Red",
     "station":"Fields Corner",
     "lat":42.300093,
-    "long":-71.061667
+    "Long":-71.061667
   },
   {
     "Line":"Red",
     "station":"Harvard Square",
     "lat":42.373362,
-    "long":-71.118956
+    "Long":-71.118956
   },
   {
     "Line":"Red",
     "station":"JFK/UMass",
     "lat":42.320685,
-    "long":-71.052391
+    "Long":-71.052391
   },
   {
     "Line":"Red",
     "station":"Kendall/MIT",
     "lat":42.36249079,
-    "long":-71.08617653
+    "Long":-71.08617653
   },
   {
     "Line":"Red",
     "station":"North Quincy",
     "lat":42.275275,
-    "long":-71.029583
+    "Long":-71.029583
   },
   {
     "Line":"Red",
     "station":"Park Street",
     "lat":42.35639457,
-    "long":-71.0624242
+    "Long":-71.0624242
   },
   {
     "Line":"Red",
     "station":"Porter Square",
     "lat":42.3884,
-    "long":-71.119149
+    "Long":-71.119149
   },
   {
     "Line":"Red",
     "station":"Quincy Adams",
     "lat":42.233391,
-    "long":-71.007153
+    "Long":-71.007153
   },
   {
     "Line":"Red",
     "station":"Quincy Center",
     "lat":42.251809,
-    "long":-71.005409
+    "Long":-71.005409
   },
   {
     "Line":"Red",
     "station":"Savin Hill",
     "lat":42.31129,
-    "long":-71.053331
+    "Long":-71.053331
   },
   {
     "Line":"Red",
     "station":"Shawmut",
     "lat":42.29312583,
-    "long":-71.06573796
+    "Long":-71.06573796
   },
   {
     "Line":"Red",
     "station":"South Station",
     "lat":42.352271,
-    "long":-71.055242
+    "Long":-71.055242
   },
   {
     "Line":"Red",
     "station":"Wollaston",
     "lat":42.2665139,
-    "long":-71.0203369
+    "Long":-71.0203369
   }
 ];
 
@@ -426,7 +426,7 @@ function createMarker()
 		var coord;
 		var color;
 		if(station.Line.toLowerCase() == line){
-			var stationLoc = new google.maps.LatLng(station.lat, station.long);
+			var stationLoc = new google.maps.LatLng(station.lat, station.Long);
 			var image = 'pinkmarker.png';
 			image.height = '40px';
 			image.width = '40px';
